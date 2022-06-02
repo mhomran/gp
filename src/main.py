e08ac6474afb0b71ff6cf25dfafefef9afaae99f
mhomran <mohamed_hassanin_omran@yahoo.com>
2022-06-02 14:34:04 +0200

    Fix: absolute pathes for the data.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
 
   np.seterr(divide='ignore', invalid='ignore')
 
-  lcap = cv.VideoCapture('/media/mhomran/Material/Workspaces/GitHub/gp/data/videos/pyrVSmas/L.mp4')
-  mcap = cv.VideoCapture('/media/mhomran/Material/Workspaces/GitHub/gp/data/videos/pyrVSmas/C.mp4')
-  rcap = cv.VideoCapture('/media/mhomran/Material/Workspaces/GitHub/gp/data/videos/pyrVSmas/R.mp4')
+  lcap = cv.VideoCapture('../data/videos/pyrVSmas/L.mp4')
+  mcap = cv.VideoCapture('../data/videos/pyrVSmas/C.mp4')
+  rcap = cv.VideoCapture('../data/videos/pyrVSmas/R.mp4')
 
   tracker = PlayerTracker(lcap, mcap, rcap)
   tracker.run()
